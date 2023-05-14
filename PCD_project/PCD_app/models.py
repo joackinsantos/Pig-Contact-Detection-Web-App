0d79786397c7b717e32cd17d93c1d72faa105023
Joackin <57931992+joackinsantos@users.noreply.github.com>
2023-05-15 05:06:43 +0800

    finished integrating detection model and interaction method to website

--- PCD_project/PCD_app/models.py
+++ PCD_project/PCD_app/models.py
@@ -7,9 +7,11 @@ from django.utils.translation import gettext_lazy as _
 class UploadImage(models.Model):
     image = models.ImageField(_("image"),upload_to='pig-images/', null=True, blank=True)  
 
-    class Meta:
-        verbose_name = "Image"
-        verbose_name_plural = "Images"
+    def __str__(self):
+        return str(os.path.split(self.image.path)[-1])
+
+class DetectedImage(models.Model):
+    image = models.ImageField(_("detected-image"),upload_to='detect-images/', null=True, blank=True)  
 
     def __str__(self):
         return str(os.path.split(self.image.path)[-1])
--- PCD_project/PCD_app/views.py
+++ PCD_project/PCD_app/views.py
@@ -1,6 +1,8 @@
 import io
 import os
+import cv2
 import torch
+import shutil
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -32,37 +34,127 @@ def upload(request):
     data = {
         'image':image,
     }
+    print(image)
     return render(request, 'upload.html', data)
 
-# this is where image detection and interaction happens
+# IMAGE DETECTION AND INTERACTION
 def results(request):
-    # getting the image
+    # GET UPLOADED IMAGE
     image = UploadImage.objects.all().last()
     img_bytes = image.image.read()
     img = im.open(io.BytesIO(img_bytes))
 
-    # we get these remotely through github
-    path_hubconfig = "../YOLOv5-Modification"
-    path_weightfile = "../best-weight/test.pt"
-
+    ## DETECTION METHOD
+    # LOADING THE MODEL (can be loaded remotely)
+    # path_hubconfig = "joackinsantos/YOLOv5-Modification:website-integration"
+    path_hubconfig = "YOLOv5-Modification"
+    path_weightfile = "best-weights/test.pt"
     model = torch.hub.load(path_hubconfig, 'custom',
                            path=path_weightfile, source='local',
                            force_reload=True)
-    
     results = model(img, size=400)
-    results.render()
 
-    for img in results.imgs:
-        img_base = im.fromarray(img)
-        img_base.save("pig-images/yolo_out/image0.jpg", format="JPEG")
+    # PROCESSING DETECTED IMAGE
+    # remove previous detected image directory to save space
+    detect_image_path = 'runs/detect'
+    exp_path = f'{detect_image_path}/exp'
+    if os.path.exists(exp_path):
+        shutil.rmtree(exp_path)
+    # creates new detected image
+    results.print()
+    results.save()
+    # move directory and change image name
+    old_path = f'{detect_image_path}/exp/image0.jpg'
+    new_path = f'{detect_image_path}/image0.jpg'
+    output_path = f'{detect_image_path}/detect-{str(image)}'
+    if not os.path.exists(new_path) and not os.path.exists(output_path):
+        shutil.move(old_path, new_path)     # move to non refreshed dir
+        os.rename(new_path, output_path)    # rename detected image
+
+    ## INTERACTION METHOD
+    # bb = bounding box
+    bb = results.pandas().xyxy[0]
+    iou_threshold = 0.07
+    results_df = pd.DataFrame(columns=['Classification', 'Interaction_Count'])
+    interaction_flag = False
+    interaction_count = 0
+
+    # separating by class names
+    head_df = bb[bb['name'] == 'Head'].reset_index(drop=True)
+    rear_df = bb[bb['name'] == 'Rear'].reset_index(drop=True)
+
+    # loop over heads and rears in the image
+    for i, head_row in head_df.iterrows():
+        for j, rear_row in rear_df.iterrows():
+            # bounding box indexing
+            # df[0] = minX, df[1] = minY, df[2] = maxX, df[3] = maxY
+
+            head_minX = head_df.iloc[i, 0]
+            head_minY = head_df.iloc[i, 1]
+            head_maxX = head_df.iloc[i, 2]
+            head_maxY = head_df.iloc[i, 3]
+
+            rear_minX = rear_df.iloc[j, 0]
+            rear_minY = rear_df.iloc[j, 1]
+            rear_maxX = rear_df.iloc[j, 2]
+            rear_maxY = rear_df.iloc[j, 3]
+
+            curr_iou = compute_iou(head_minX, head_maxX, head_minY, head_maxY,
+                        rear_minX, rear_maxX, rear_minY, rear_maxY)
+            
+            if(curr_iou >= iou_threshold):
+                interaction_flag = True
+                interaction_count += 1
     
-    inference_img = "pig-images/yolo_out/image0.jpg"
+    temp_list = [1 if interaction_flag else 0,
+                 interaction_count]
+    results_df.loc[len(results_df)] = temp_list
+
+    row = results_df.iloc[0]
+    classification = 'With Contact' if row['Classification'] == 1 else "Without Contact"
+    count = row['Interaction_Count']
 
     data = {
-        'image':inference_img,
+        'image': f'../{output_path}',
+        'class': classification,
+        'interaction_count': count
     }
 
     return render(request, 'results.html', data)
 
 def test(request):
     return render(request, 'test.html')
+
+
+
+# FUNCTIONS
+def compute_iou(head_minX, head_maxX, head_minY, head_maxY, rear_minX, rear_maxX, rear_minY, rear_maxY):
+    # determine (x,y) coordinates of the intersection rectangle
+    x_left = max(head_minX, rear_minX)
+    y_top = max(head_minY, rear_minY)
+    x_right = min(head_maxX, rear_maxX)
+    y_bottom = min(head_maxY, rear_maxY)
+
+    # compute the area of intersection rectangle
+    intersection_area = max(0, x_right - x_left + 1) * max(0, y_bottom - y_top + 1)
+
+    # compute area of both prediction and ground-truth
+    # rectangles
+    headArea = (head_maxX - head_minX + 1) * (head_maxY - head_minY + 1)
+    rearArea = (rear_maxX - rear_minX + 1) * (rear_maxY - rear_minY + 1)
+    union_area = float(headArea + rearArea - intersection_area)
+
+    # compute IoU
+    iou = intersection_area / union_area
+    return iou
+
+# USEFUL TESTERS
+# Tester for Head and Rear dfs
+# use to test order
+# print(head_df.head())
+# print(rear_df.head())
+# head_minX = head_df.iloc[0, 0]
+# head_minY = head_df.iloc[0, 1]
+# head_maxX = head_df.iloc[0, 2]
+# head_maxY = head_df.iloc[0, 3]
+# print(head_minX, head_minY, head_maxX, head_maxY)
