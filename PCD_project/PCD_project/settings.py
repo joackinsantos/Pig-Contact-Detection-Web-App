a3dd77b1f5af7024d77aa6e3017127796fe662fe
Joackin <57931992+joackinsantos@users.noreply.github.com>
2023-05-26 23:21:45 +0800

    changed comment

--- PCD_project/PCD_project/settings.py
+++ PCD_project/PCD_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "PCD_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# Local DB
+# Local Database
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
